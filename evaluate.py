4dbd7d3b68324b277c453581f448f549f0af1a5c
hudsonmartins <hudsonbr95@gmail.com>
2025-07-22 07:08:29 -0300

    feat: add benchmark results for ORB-SLAM2 and update evaluation script

--- evaluate.py
+++ evaluate.py
@@ -1,24 +1,34 @@
+import evo.main_ape as main_ape
+import evo.main_rpe as main_rpe
+
 from evo.core import metrics
 from evo.tools import file_interface
 from evo.core import sync
 
+def get_rte(traj_ref, traj_est, correct_scale=False):
+    return main_rpe.rpe(traj_ref, traj_est, 
+                          pose_relation=metrics.PoseRelation.translation_part,
+                          align_origin=True,
+                          align=True,
+                          correct_scale=correct_scale,
+                          delta=1, delta_unit=metrics.Unit.frames) 
 
-def get_metrics(ref_file, est_file):
-    traj_ref = file_interface.read_tum_trajectory_file(ref_file)
-    traj_est = file_interface.read_tum_trajectory_file(est_file)
-   
-    max_diff = 0.01
-    traj_ref, traj_est = sync.associate_trajectories(traj_ref, traj_est, max_diff)
-    traj_est.align(traj_ref, correct_scale=True, correct_only_scale=False)
-    data = (traj_ref, traj_est)
+def get_roe(traj_ref, traj_est, correct_scale=False):
+    return main_rpe.rpe(traj_ref, traj_est, 
+                          pose_relation=metrics.PoseRelation.rotation_angle_deg,
+                          align_origin=True,
+                          align=True,
+                          correct_scale=correct_scale,
+                          delta=1, delta_unit=metrics.Unit.frames)
 
-    #APE
-    pose_relation = metrics.PoseRelation.translation_part
-    ate_metric = metrics.APE(pose_relation)
-    ate_metric.process_data(data)
-    ate_stat = ate_metric.get_statistic(metrics.StatisticsType.rmse)
-    print('ATE: ', round(ate_stat, 2))
-    return ate_stat
+
+def get_ate(traj_ref, traj_est, correct_scale=False):
+    return main_ape.ape(traj_ref, traj_est, 
+                          pose_relation=metrics.PoseRelation.translation_part,
+                          align_origin=True,
+                          align=True,
+                          correct_scale=correct_scale)
+
 
 
 if __name__ == '__main__':
@@ -26,11 +36,27 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('ref_file', help='Ground truth file (it needs to be a in TUM format)')
     parser.add_argument('est_file', help='Odometry estimation (it needs to be a in TUM format)')
+    parser.add_argument('--correct_scale', action='store_true', help='Correct scale of the trajectory')
     parser.add_argument('--output_path', help='Path to the output file (default: None)')
     args = parser.parse_args()
-    ate = get_metrics(args.ref_file, args.est_file)
+
+
+    traj_ref = file_interface.read_tum_trajectory_file(args.ref_file)
+    traj_est = file_interface.read_tum_trajectory_file(args.est_file)
+   
+    max_diff = 0.01
+    traj_ref, traj_est = sync.associate_trajectories(traj_ref, traj_est, max_diff)
+
+
+    ate = get_ate(traj_ref=traj_ref, traj_est=traj_est, correct_scale=args.correct_scale)
+    rte = get_rte(traj_ref=traj_ref, traj_est=traj_est, correct_scale=args.correct_scale)
+    roe = get_roe(traj_ref=traj_ref, traj_est=traj_est, correct_scale=args.correct_scale)
+    print('ATE (m):', ate)
+    print('RTE (m/frame):', rte)
+    print('ROE (deg/frame):', roe)
+        
     if(args.output_path):
         with open(args.output_path, 'w') as f:
-            f.write('Ground-truth file: ' + args.ref_file+'\n') 
-            f.write('Estimation file: ' + args.est_file+'\n')
-            f.write(' ATE: ' + str(ate)+' (m)\n')
+            f.write(f'ATE (m): {ate.get_all_statistics().rmse}\n')
+            f.write(f'RTE (m/frame): {rte.get_all_statistics().rmse}\n')
+            f.write(f'ROE (deg/frame): {roe.get_all_statistics().rmse}\n')
